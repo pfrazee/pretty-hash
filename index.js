5ddcbd96fc11e1762c095528ef25908e9cf006dd
Paul Frazee <pfrazee@gmail.com>
2016-11-22 11:58:15 -0600

    support string inputs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 module.exports = function prettyHash (buf) {
-  if (Buffer.isBuffer(buf)) {
-    buf = buf.toString('hex')
-    if (buf.length > 8) return buf.slice(0, 6) + '..' + buf.slice(-2)
+  if (Buffer.isBuffer(buf)) buf = buf.toString('hex')
+  if (typeof buf === 'string' && buf.length > 8) {
+    return buf.slice(0, 6) + '..' + buf.slice(-2)
   }
   return buf
 }
